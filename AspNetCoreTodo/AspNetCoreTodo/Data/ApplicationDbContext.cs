18f8d0ddbb42898337e007b04fef60dd24639aef
Nikolai Sison <sisonnikolai@gmail.com>
2020-10-09 17:47:15 +0800

    Initialized project

---
+++ AspNetCoreTodo/AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreTodo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
