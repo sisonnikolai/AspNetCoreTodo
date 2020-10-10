686e3e0e5867fde108b639ea75b247427bd4d1ca
Nikolai Sison <sisonnikolai@gmail.com>
2020-10-10 14:02:15 +0800

    Implemented the add item function

--- AspNetCoreTodo/AspNetCoreTodo/Services/ITodoItemService.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Services/ITodoItemService.cs
@@ -9,5 +9,6 @@ namespace AspNetCoreTodo.Services
     public interface ITodoItemService
     {
         Task<TodoItem[]> GetIncompleteItemsAsync();
+        Task<bool> AddItemAsync(TodoItem newItem);
     }
 }
--- AspNetCoreTodo/AspNetCoreTodo/Services/TodoItemService.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Services/TodoItemService.cs
@@ -23,5 +23,21 @@ namespace AspNetCoreTodo.Services
                 .Where(x => x.IsDone == false)
                 .ToArrayAsync();
         }
+
+        public async Task<bool> AddItemAsync(TodoItem newItem)
+        {
+            newItem.Id = Guid.NewGuid();
+            newItem.IsDone = false;
+
+            if (newItem.DueAt == null)
+            {
+                newItem.DueAt = DateTimeOffset.Now.AddDays(3);
+            }
+
+            _context.Items.Add(newItem);
+
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1;
+        }
     }
 }
